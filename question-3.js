f0a291b76b200fe788df4938b8a7835d5a7fd891
Pop <shounen.pop@gmail.com>
2024-07-30 10:16:34 +0700

    feat: add ans ex1-5

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
-
+Answer: O(n) เพราะ linear search มีการเข้าถึงข้อมูลทีละ index ไล่ไปจนครบจำนวน data ใน array
+แต่ข้อนี้จริง ๆ ก็ใช้ binary search ได้นะครับ เห็นไอดีเรียงมาแล้ว แต่โจทย์บังคับ linear 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
-
+Answer: O(log n) เพราะ data ใน array ในส่วนของ name เรียงลำดับมาแล้ว 
+เลยใช้ binary search เพื่อลดจำนวนของการลูปลงครึ่งนึงในทุก ๆ รอบลูป
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
-
+Answer: findProductPrice ที่ใช้ O(log n) ดีกว่า เพราะมีการลดจำนวนลูปลงทีละครึ่ง 
+ต่างจาก findStudentById ที่ใช้ linear search ซึ่งจะลูปเข้าถึงทีละ index เพื่อเช็คค่า
 
 */
--- question-4.js
+++ question-4.js
@@ -1,5 +1,17 @@
 function sortProductsByPrice(products) {
+  // return products.sort((a, b) => a.price - b.price);
+
   // Your code here
+  for (let i = 0; i < products.length; i++) {
+    for (let j = 0; j < products.length - i - 1; j++) {
+      if (products[j].price > products[j + 1].price) {
+        // let temp = products[j];
+        // products[j] = products[j + 1];
+        // products[j + 1] = temp;
+      }
+    }
+  }
+  return products;
 }
 
 // Test case
